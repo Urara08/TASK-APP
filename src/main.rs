6cecb23a9125c65fecfe461748c809a999cf00df
Urara <90606372+Urara08@users.noreply.github.com>
2026-01-22 22:58:52 +0900

    Add files via upload
    コメントの//の右側のスペースを削除
    コメント追記

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,11 @@ file_path.read_to_string(&mut contents).unwrap();
 let mut lines: Vec<String> = Vec::new();
 
 //data.txtの生データからVecの中身を形成
-let mut lines: Vec<String> = contents// (Vecをシャドーイング：contents⇒lines)
+let mut lines: Vec<String> = contents//(Vecをシャドーイング：contents⇒lines)
 .lines()//改行で分割
 .filter(|&line| !&line.trim().is_empty()) //Vecの空行を除外
 .map(String::from)//Vec<String>に分割
-.collect();
+.collect();//Vecに格納
 
 //未完了タスクの表示
 if lines.len() == 0 {
@@ -36,41 +36,40 @@ if lines.len() == 0 {
     }
 
 //ナンバリングしたlinesを表示
-    for (index, line) in &mut lines.clone().into_iter().enumerate()  {// インデックスと各要素のペアをタプルにして取得
-    let line = line; // linesから文字列を取り出す
-    println!("{}:{}", index + 1, line); // 行番号 (1から開始) と行文字列を出力
+    for (index, line) in &mut lines.clone().into_iter().enumerate()  {//インデックスと各要素のペアをタプルにして取得
+    let line = line; //linesから文字列を取り出す
+    println!("{}:{}", index + 1, line); //行番号 (1から開始) と行文字列を出力
     }
 
 //処理番号を標準入力で取得
-let mut Service_type = String::new();
-io::stdin().read_line(&mut Service_type).unwrap();
-Service_type.trim().to_string().parse::<u32>().unwrap();
-println!("{}",Service_type);
+let mut Service_type = String::new();//入力された処理番号を格納
+io::stdin().read_line(&mut Service_type).unwrap();//標準入力で取得
+Service_type.trim().to_string().parse::<u32>().unwrap();//整数に変換
 
 //処理番号で分岐
 if Service_type.trim() == "0"{
 
 //０：タスクの登録へ
     println!("新規タスクを入力してください");
-    let mut Register_name = String::new();// 入力されたタスク名を格納
-    io::stdin().read_line(&mut Register_name).unwrap();// 標準入力で取得
-    Register_name.trim().to_string().parse::<String>().unwrap();// 文字列に変換
+    let mut Register_name = String::new();//入力されたタスク名を格納
+    io::stdin().read_line(&mut Register_name).unwrap();//標準入力で取得
+    Register_name.trim().to_string().parse::<String>().unwrap();//文字列に変換
 
 //追加足したタスクをdata.txtに追記する関数へ
     file_add_register(Register_name);
 
 }else if
 //１：タスクの削除へ
-    Service_type.trim() == "1" && lines.len() != 0{// 残っているタスクがある場合
+    Service_type.trim() == "1" && lines.len() != 0{//残っているタスクがある場合
     println!("完了したタスク番号を入力してください");
-    let mut task_number = String::new();// 入力されたタスク番号を格納
-    io::stdin().read_line(&mut task_number).unwrap();// 標準入力で取得
-    let task_index = task_number.trim().parse::<usize>().unwrap() - 1;// Vecは0始まりのため-1
+    let mut task_number = String::new();//入力されたタスク番号を格納
+    io::stdin().read_line(&mut task_number).unwrap();//標準入力で取得
+    let task_index = task_number.trim().parse::<usize>().unwrap() - 1;//Vecは0始まりのため-1
 
 //指定されたタスク番号のタスクを削除する処理へ
-let mut f = File::open("src/data.txt").unwrap();// ファイルを開く
-if  lines.len() > task_index{// 指定されたタスク番号がタスク数を超えていない場合
-    lines.remove(task_index);// Vecから指定されたタスク番号のタスクを削除
+let mut f = File::open("src/data.txt").unwrap();//ファイルを開く
+if  lines.len() > task_index{//指定されたタスク番号がタスク数を超えていない場合
+    lines.remove(task_index);//Vecから指定されたタスク番号のタスクを削除
     println!("未完了のタスク{:?}",&lines);
 
 //削除後のタスク一覧をdata.txtに書き込む関数へ
