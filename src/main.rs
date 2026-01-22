9548df8fb5331c3772349f5488c677a49d22f90f
Urara <90606372+Urara08@users.noreply.github.com>
2026-01-22 11:38:07 +0900

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,31 @@
+use std::io;//標準入力
+use std::fs::OpenOptions;//ファイル追記
 use std::fs::File;//ファイル操作用
-use std::io::prelude::*;//ファイルの入出力用
-use std::io;//標準入力用
-use std::fs::OpenOptions;//ファイル追記用
-use std::io::{Read, Write, BufWriter};//ファイル
+use std::io::{Read, Write, BufWriter};//ファイル入出力
+use std::io::prelude::*;//ファイル入出力
+
 
 mod register;//新規タスク登録用モジュール 宣言
 mod delete;//タスク削除用モジュール 宣言
-//use crate::register::呼び出す関数名;
-//use crate::delete::呼び出す関数名;
+use register::file_add_register;//新規タスク登録用モジュールの関数をインポート 
 
 fn main(){
 
-let mut f:File= File::open("src/data.txt").unwrap();
+let mut file_path:File= File::open("src/data.txt").unwrap();
 
 let mut contents = String::new();
 
 //ファイルの内容を1行ずつ読み込む
-f.read_to_string(&mut contents).unwrap();
+file_path.read_to_string(&mut contents).unwrap();
 
 let mut lines: Vec<String> = Vec::new();
 
 //改行でVec<String>に分割
+//生データからVecの中身を形成
+// (Vecをシャドーイング：contents⇒lines)
 let mut lines: Vec<String> = contents
 .lines()
-.filter(|&line| !&line.trim().is_empty()) // ← ★ 空行を除外
+.filter(|&line| !&line.trim().is_empty()) // ← Vecの空行を除外
 .map(String::from)
 .collect();
 
@@ -35,13 +37,14 @@ if lines.len() == 0 {
         println!("処理を選択してください\n(０:新規タスク登録、１:タスクの完了)");
     }
 
-//ナンバリングしたlinesを表示(Rust Vec 自動でナンバリング?)
+//ナンバリングしたlinesを表示
     for (index, line) in &mut lines.clone().into_iter().enumerate()  {
     let line = line; // linesから文字列を取り出す
     println!("{}:{}", index + 1, line); // 行番号 (1から開始) と行文字列を出力
     }
 
-//処理番号を入力
+//処理番号を標準入力で取得
+//入力を受け取る箱を作る
 let mut Service_type = String::new();
 io::stdin().read_line(&mut Service_type).unwrap();
 
@@ -58,19 +61,11 @@ Register_name.trim().to_string().parse::<String>().unwrap();
 
 
 //Register_nameをdata.txtに追記する処理へ
-let mut f = OpenOptions::new()
-.create(true)
-.append(true)     // 追記モードにする
-.open("src/data.txt")
-.expect("ファイルを開けませんでした");
-
-writeln!(f, "{}", Register_name).unwrap();
-println!("新規タスクの登録が完了しました");
+file_add_register(file_path,Register_name);
 
 
 }else if
-//タスクの削除へ
-//処理中
+
 
     Service_type.trim() == "1" && lines.len() != 0{
         println!("完了したタスク番号を入力してください");
@@ -86,17 +81,17 @@ println!("新規タスクの登録が完了しました");
         let mut f = File::open("src/data.txt").unwrap();
         if lines.len() > task_index{
             lines.remove(task_index);
-            println!("削除後Vec{:?}",&lines);//Vecが正しい状態かデバック用に確認
+            println!("残りのタスク{:?}",&lines);//Vecが正しい状態かデバック用に確認
             //ファイルを一旦空にする
-            let mut f = BufWriter::new(File::create("src/data.txt").unwrap());
+            let mut file_path = BufWriter::new(File::create("src/data.txt").unwrap());
 
-            let mut f = OpenOptions::new()
+            let mut file_path = OpenOptions::new()
             .create(true)
             .append(true)
             .open("src/data.txt")
             .expect("ファイルを開けませんでした");
 
-            writeln!(f, "{}", &lines.join("\n")).unwrap();
+            writeln!(file_path, "{}", &lines.join("\n")).unwrap();
             println!("タスクの削除が完了しました");
 
         }else {println!("エラー: 指定された番号は範囲外です");}
@@ -108,4 +103,3 @@ println!("新規タスクの登録が完了しました");
             return;
         }
 }
-
--- src/register.rs
+++ src/register.rs
@@ -1,1 +1,18 @@
-//新規タスク登録用モジュール
+use std::io;
+use std::fs::OpenOptions;
+use std::fs::File;
+use std::io::{Read, Write, BufWriter};
+
+//登録内容をdata.txtに追記する関数
+//x=file_path, y=Register_name
+
+pub fn file_add_register(x:File,y:String){
+let mut x = OpenOptions::new()
+.create(true)
+.append(true)
+.open("src/data.txt")
+.expect("ファイルを開けませんでした");
+
+writeln!(x, "{}", y).unwrap();
+println!("新規タスクの登録が完了しました");
+}
